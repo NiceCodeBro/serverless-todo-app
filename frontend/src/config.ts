e822e6688d1eb633f8bf0d181205d3ae8233ad28
Selim Dursun <m.seldur@gmail.com>
2021-01-16 14:25:21 +0100

    frontend: auth0 configurations(domain, cliendId..) is done

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,9 +1,9 @@
-const apiId = ''
-const apiRegion = ''
+const apiId = 'ortw10h3ug'
+const apiRegion = 'eu-centra-1'
 export const apiEndpoint = `https://${apiId}.execute-api.${apiRegion}.amazonaws.com/dev`
 
 export const authConfig = {
-  domain: '',                   // Auth0 domain
-  clientId: '',          // Auth0 client id
+  domain: 'dev-xx-selim.eu.auth0.com',                   // Auth0 domain
+  clientId: 'TqWNsK6DNc2TNIT3zPVMfGb2p3KxC6pF',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
