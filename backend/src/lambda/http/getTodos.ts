bd097afba0d460d097faa351d2689cf73041ea4a
Selim Dursun <m.seldur@gmail.com>
2021-01-16 19:56:52 +0100

    getTodos handler is implemented

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,7 +1,55 @@
 import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { parseUserId } from '../../auth/utils';
+
+var AWS = require("aws-sdk");
+
+
+var docClient = new AWS.DynamoDB.DocumentClient();
+
+const todosTableName = process.env.TODOS_TABLE;
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  // TODO: Get all TODO items for a current user
+
+  const authHeader = event.headers.Authorization
+  const authSplit = authHeader.split(" ")
+  const userId = parseUserId(authSplit[1])
+
+  const result = await docClient.query({
+    TableName : todosTableName,
+    IndexName: "UserIdIndex",
+    KeyConditionExpression: 'userId = :userId',
+    ExpressionAttributeValues: {
+        ':userId': userId
+    },
+
+    ScanIndexForward: false
+  }).promise()
+
+  if (result) {
+    console.log('Result: ', result)
+
+    const items = result.Items
+
+    return {
+        statusCode: 200,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': true
+        },
+        body: JSON.stringify({
+            items
+        })
+    }
+  }
+
+  return {
+    statusCode: 404,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: ''
+  };
+
 }
