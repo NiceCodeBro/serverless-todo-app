dedec7a3b1f8fc80b87e696bb0ca6acbe7056069
Selim Dursun <m.seldur@gmail.com>
2021-01-17 16:51:11 +0100

    deleteToDo is implemented

--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -1,10 +1,46 @@
 import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { getUserId } from '../utils';
+
+const AWS = require("aws-sdk");
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+const todosTableName = process.env.TODOS_TABLE;
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const todoId = event.pathParameters.todoId
+  console.log('Processing event: ', event);
 
-  // TODO: Remove a TODO item by id
-  return undefined
+  const todoId = event.pathParameters.todoId;
+  const userId = getUserId(event);
+
+  const todoItemToDelete = {
+    TableName: todosTableName,
+    Key:{
+        "todoId": todoId,
+        "userId": userId
+    }
+  }
+
+  try{
+    await docClient.delete(todoItemToDelete).promise()
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: ''
+   };
+  } catch(err) {
+    console.error("Unable to delete item. Error JSON:", JSON.stringify(err, null, 2));
+      return {
+        statusCode: 400,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true
+        },
+        body: ''
+      };
+  }
 }
