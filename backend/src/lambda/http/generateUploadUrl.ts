c0e09c804b309eb38e15045777dced9f41366258
Selim Dursun <m.seldur@gmail.com>
2021-01-17 16:56:08 +0100

    signed s3 url generator func is implemented

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -1,10 +1,48 @@
 import 'source-map-support/register'
+import * as uuid from 'uuid';
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 
+
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3({
+  signatureVersion: 'v4'
+});
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+const todosTableName = process.env.TODOS_TABLE;
+const todosImagesTableName = process.env.TODOS_TABLE;
+const todosImagesBucketName = process.env.TODOS_S3_BUCKET;
+const signedUrlExpireSeconds = process.env.SIGNED_URL_EXPIRATION;
+
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  console.log('Processing event: ', event);
+
   const todoId = event.pathParameters.todoId
 
-  // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-  return undefined
+  //const imageId:uuid = uuid.v4();
+  
+  const presignedUrl = await getPutSignedUrl(todoId);
+
+  console.log("presignedURL as in generateuploadurl:", presignedUrl);
+  
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify({
+        uploadUrl: presignedUrl
+    })
+  };
+}
+
+// Generates an AWS signed URL for uploading objects
+async function getPutSignedUrl( key: string ): Promise <string> {
+  return s3.getSignedUrl('putObject', {
+    Bucket: todosImagesBucketName,
+    Key: key,
+    Expires: signedUrlExpireSeconds,
+  });
 }
