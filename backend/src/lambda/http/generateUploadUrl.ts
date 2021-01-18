7c81f9d4e1941323ed6323cf1569e9ff03080348
Selim Dursun <m.seldur@gmail.com>
2021-01-18 22:12:04 +0100

    signed image url is attached to todo entry

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -2,6 +2,7 @@ import 'source-map-support/register'
 import * as uuid from 'uuid';
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { getUserId } from '../utils';
 
 
 const AWS = require("aws-sdk");
@@ -18,11 +19,10 @@ const signedUrlExpireSeconds = process.env.SIGNED_URL_EXPIRATION;
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   console.log('Processing event: ', event);
 
-  const todoId = event.pathParameters.todoId
-
-  //const imageId:uuid = uuid.v4();
-  
+  const todoId = event.pathParameters.todoId  
   const presignedUrl = await getPutSignedUrl(todoId);
+
+  await attachUrl(event, getImageUrl(todoId), todoId);
 
   console.log("presignedURL as in generateuploadurl:", presignedUrl);
   
@@ -43,6 +43,39 @@ async function getPutSignedUrl( key: string ): Promise <string> {
   return s3.getSignedUrl('putObject', {
     Bucket: todosImagesBucketName,
     Key: key,
-    Expires: signedUrlExpireSeconds,
+    Expires: parseInt(signedUrlExpireSeconds),
   });
+}
+
+async function attachUrl(event: any, url: string, todoId: string) {
+  const userId = getUserId(event);
+
+  const params = {
+    TableName: todosTableName,
+    Key: {
+      "todoId": todoId,
+      "userId": userId
+    },
+    UpdateExpression: "set #attachmentUrl = :attachmentUrl",
+    ExpressionAttributeValues: {
+      ":attachmentUrl": url
+    },
+    ExpressionAttributeNames: {
+      "#attachmentUrl": "attachmentUrl"
+    },
+    ReturnValues: "UPDATED_NEW"
+  }
+
+  await docClient.update(params, function(err, data) {
+    if (err) {
+        console.error("Unable to update item. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("UpdateItem succeeded:", JSON.stringify(data, null, 2));
+    }
+  }).promise();
+}
+
+function getImageUrl(todoId: string) {
+  return `https://${todosImagesBucketName}.s3.amazonaws.com/${todoId}`
+
 }
